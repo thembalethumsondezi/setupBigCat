d19f43e42b3c6b6bf4e003d539460ef1d881edf8
nangamsosibhinyalaza <166251005+nangamsosibhinyalaza@users.noreply.github.com>
2024-05-10 06:35:44 +0200

    Create tiger.py

---
+++ tiger.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import subprocess
+
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/nangamsosibhinyalaza/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
