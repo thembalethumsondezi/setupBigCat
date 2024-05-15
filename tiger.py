76cde0c2c773a5083ca1afa2c86b06cc91ee8d6b
silumkomakhabela <149713438+silumkomakhabela@users.noreply.github.com>
2024-05-15 21:42:05 +0200

    Update tiger.py

--- tiger.py
+++ tiger.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/nangamsosibhinyalaza/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/silumkomakhabela/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
