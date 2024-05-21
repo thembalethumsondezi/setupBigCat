e08bdfb8698cdd431ca91afd10959696b83940f8
thembalethumsondezi <thembalethumsondezi@gmail.com>
2024-05-21 07:57:39 +0200

    Update tiger.py

--- tiger.py
+++ tiger.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/buyilechithibhunga/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/thembalethumsondezi/tiger.git;cd tiger;chmod +x tiger;bash tiger", shell=True)
